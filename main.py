c23446335c17175db19435386fb8d061d546268c
Lemasc Service <lemascserviceth@hotmail.com>
2024-11-09 02:20:50 +0700

    not sure about it

--- main.py
+++ main.py
@@ -6,40 +6,45 @@ from deep_sort_realtime.deepsort_tracker import DeepSort
 import datetime
 from collections import OrderedDict
  
-cap = cv2.VideoCapture(r'D:\Coding\train-poon-poon\tobeit-68-alpr\video.mp4')
+cap = cv2.VideoCapture(r'D:\Coding\train-poon-poon\alpr\2103099-hd_1920_1080_30fps.mp4')
 
-model = YOLO(r'D:\Coding\train-poon-poon\tobeit-68-alpr\license-plate-final-best.pt')
+model = YOLO(r'D:\Coding\train-poon-poon\alpr\license-plate-final-best.pt')
 tracker = DeepSort(max_age=50)
-reader = easyocr.Reader(['th','en'])
+reader = easyocr.Reader(['en'])
 
 fps = cap.get(cv2.CAP_PROP_FPS)
 
-CONFIDENCE_THRESHOLD = 0.8
+CONFIDENCE_THRESHOLD = 0.6
 GREEN = (0, 255, 0)
 WHITE = (255, 255, 255)
 
 ocr_results = OrderedDict()
 
-def run_ocr(plate, trackId):
-    if ocr_results.get(trackId):
+def run_ocr(plate, trackId, plateConfidence = 0):
+    existing = ocr_results.get(trackId)
+    if existing and plateConfidence and existing["confidence"] > plateConfidence:
         ocr_results.move_to_end(trackId, last=False)
-        return ocr_results[trackId]
+        return ocr_results[trackId]["text"]
     try:
         crop_gray = cv2.cvtColor(plate, cv2.COLOR_BGR2GRAY)
-        # _, crop_thresh = cv2.threshold(crop_gray, 64, 255, cv2.THRESH_BINARY_INV)
-        cv2.imshow('plate', crop_gray)
-        results = reader.readtext(crop_gray)
+        _, crop_thresh = cv2.threshold(crop_gray, 70, 255, cv2.THRESH_BINARY)
+        # cv2.imshow('plate', crop_thresh)
+        results = reader.readtext(crop_thresh)
         final_text = []
         for result in results:
             _, text, score = result
-            if float(score) > 0.5:
-                final_text.append(text)
+            final_text.append(text)
         
-        final_text = " ".join(final_text)
-        if len(final_text) > 1:
+        if len(final_text) >= 1:
+            final_text = " ".join(final_text)
             if len(ocr_results) > 20:
                 ocr_results.popitem(last=False)
-            ocr_results[trackId] = final_text
+            if plateConfidence is None:
+                plateConfidence = 0
+            ocr_results[trackId] = {
+                "confidence": plateConfidence,
+                "text": final_text
+            }
             return final_text
         
         return None
@@ -75,8 +80,8 @@ def process_frame():
             results.append([[x1, y1, w, h], conf, classId])
 
             # Print the confidence over the rectangle
-            # cv2.putText(frame, f"{conf:0.2f}", (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
-            # cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.putText(frame, f"{conf:0.2f}", (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
             # Crop the license plate
             # plate = frame[y1:y2, x1:x2]
@@ -92,32 +97,37 @@ def process_frame():
         # cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
 
-    # update the tracker with the new detections
-    tracks = tracker.update_tracks(results, frame=frame)
+    # # update the tracker with the new detections
+    # tracks = tracker.update_tracks(results, frame=frame)
 
-     # loop over the tracks
-    for track in tracks:
-        # if the track is not confirmed, ignore it
-        if not track.is_confirmed():
-            continue
+    #  # loop over the tracks
+    # for track in tracks:
+        # # if the track is not confirmed, ignore it
+        # if not track.is_confirmed():
+        #     continue
 
-        # get the track id and the bounding box
-        track_id = track.track_id
-        ltrb = track.to_ltrb()
+        # # get the track id and the bounding box
+        # track_id = track.track_id
+        # ltrb = track.to_ltrb()
 
-        xmin, ymin, xmax, ymax = [int(x) for x in ltrb]
+        # xmin, ymin, xmax, ymax = [int(x) for x in ltrb]
 
-        plate = frame[ymin:ymax, xmin:xmax, :]
-        if plate.size < 1:
-            continue
+        # plate = frame[ymin:ymax, xmin:xmax, :]
+        # if plate.size < 1:
+        #     continue
 
-        # draw the bounding box and the track id
-        cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), GREEN, 2)
-        cv2.rectangle(frame, (xmin, ymin - 20), (xmin + 20, ymin), GREEN, -1)
-        cv2.putText(frame, str(track_id), (xmin + 5, ymin - 8),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, WHITE, 1)
         
-        text = run_ocr(plate, track_id)
+        # text = run_ocr(plate, track_id, track.det_conf)
+
+        # if text:
+        #     # print(f"Track ID: {track_id}, Text: {text}")
+
+        #     # draw the bounding box and the track id
+        #     cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), GREEN, 2)
+        #     cv2.rectangle(frame, (xmin, ymin - 20), (xmin + 20, ymin), GREEN, -1)
+        #     cv2.putText(frame, text, (xmin + 5, ymin - 8),
+        #                 cv2.FONT_HERSHEY_SIMPLEX, 0.5, WHITE, 1)
+        
         # print(text)
         # if text:
         #     cv2.putText(frame, text[0], (xmin, ymin - 40),
